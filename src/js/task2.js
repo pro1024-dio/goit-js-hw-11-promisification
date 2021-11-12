8dbec871f27519f1571499af7cf740226637ff21
Ivan Dernovyi <idernovoy@gmail.com>
2021-11-12 21:53:44 +0200

    project
    Task3 completed

---
+++ src/js/task2.js
@@ -0,0 +1,34 @@
+import '../sass/main.scss';
+
+console.log("Script for task2 connect");
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName, callback) => {
+  const updatedUsers = allUsers.map(user =>
+    user.name === userName ? { ...user, active: !user.active } : user,
+  );
+
+//   callback(updatedUsers);
+
+    return new Promise((resolve) => {resolve(updatedUsers)});
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+/*
+ * Сейчас работает так
+ */
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
